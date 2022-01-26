81a259f48a427444e5f94cae1d505a35a93cf798
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2022-01-25 22:15:21 -0300

    feat: inicio navbar responsivo

--- apae/src/app/app.component.ts
+++ apae/src/app/app.component.ts
@@ -11,15 +11,4 @@ export class AppComponent {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
-  myFunction() {
-    console.log('funcao');
-    let topnav: any = document.getElementById("myTopnav");
-    
-    if (topnav.className == "navbar topnav") {
-      topnav.className += " responsive";
-    } else {
-      topnav.className = "navbar topnav";
-    }
-  }
-
 }
--- apae/src/app/home/home.component.ts
+++ apae/src/app/home/home.component.ts
@@ -16,4 +16,15 @@ export class HomeComponent implements OnInit {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
+  myFunction() {
+    console.log('funcao');
+    let topnav: any = document.getElementById("myTopnav");
+    
+    if (topnav.className == "topnav") {
+      topnav.className += " responsive";
+    } else {
+      topnav.className = "topnav";
+    }
+  }
+
 }
