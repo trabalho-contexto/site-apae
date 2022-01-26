bcb0caf18b76fbd397d14e513613b08c75fe2111
Leonam Teixeira de Vasconcelos <leonam.teixeira.vasconcelos@gmail.com>
2022-01-25 21:32:46 -0300

    feat: iniciando página de feed #3

--- apae/src/app/app.module.ts
+++ apae/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
