37791730e1ebacd1eeebbd5086932e3442d67291
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2022-01-25 21:54:48 -0300

    feat: trocando componentes

--- apae/src/app/home/home.component.ts
+++ apae/src/app/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  scrollTo ( element: any ) {
+    element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
 }
