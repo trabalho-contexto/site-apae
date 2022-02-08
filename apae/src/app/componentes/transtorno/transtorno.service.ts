75f76b54c9cdc41e43484b46ceb5061feacf24ec
diegopiovezana <diegopiovezana@gmail.com>
2022-02-07 21:39:53 -0300

    fix: atualizando transtornos DSM-5

--- apae/src/app/componentes/transtorno/transtorno.service.ts
+++ apae/src/app/componentes/transtorno/transtorno.service.ts
@@ -11,62 +11,59 @@ export class TranstornoService {
   getTranstornos () : Array<Transtorno>{
     return [
       new Transtorno(
-        'Retardo Mental Leve (F70)',
-        `Nesse tipo de retardo mental, as pessoas com um grau leve de retardo mental podem atingir um nível de leitura similar aos das crianças que cursam entre a 4ª e a 6ª série escolar, aprender as habilidades educacionais básicas necessárias no dia-a-dia.
-        Essas pessoas geralmente não apresentam defeitos físicos evidentes, mas podem apresentar epilepsia e necessitam de supervisão de instituições educacionais especiais. São frequentemente imaturos e pouco refinados, com pouca capacidade de interação social. A sua linha de pensamento é muito específica e em geral, eles são incapazes de generalizar. Possuem dificuldades para ajustar-se a situações novas e podem apresentar uma má capacidade de julgamento, falta de prevenção e credulidade excessiva, e são capazes de cometer atos impulsivos.`,
-        `https://www.tuasaude.com/retardo-mental-leve/`
+        'Desordem do Desenvolvimento Intelectual (6A00.Z)',
+        `Distúrbios do desenvolvimento intelectual são um grupo de condições etiologicamente diversas originadas durante o período de desenvolvimento, caracterizadas por funcionamento intelectual significativamente abaixo da média e comportamento adaptativo que estão aproximadamente dois ou mais desvios-padrão abaixo da média (aproximadamente menos do que o percentil 2,3), com base em normalizados, testes padronizados administrados individualmente. Quando os testes adequadamente normatizados e padronizados não estão disponíveis, o diagnóstico de distúrbios do desenvolvimento intelectual requer maior confiança no julgamento clínico baseado na avaliação apropriada de indicadores comportamentais comparáveis.`,
+        `https://icd.who.int/browse11/l-m/en#/http%3a%2f%2fid.who.int%2ficd%2fentity%2f605267007`
       ),
       new Transtorno(
-        'Retardo Mental Moderado (F71)',
+        'Desordem do Desenvolvimento Intelectual moderado (6A00.Z)',
         `O retardo mental moderado é quando a pessoa apresenta um quociente de inteligência (QI) entre 35 e 55. A pessoa apresenta uma maior lentidão para aprender a falar ou sentar, mas se receber treinamento e apoio adequados, os adultos com esse grau de retardo mental conseguem viver com alguma independência. A intensidade do apoio deve ser estabelecida para cada paciente e algumas vezes pode ser preciso apenas uma pequena ajuda, para que consiga estar integrado.`,
         `https://www.tuasaude.com/retardo-mental-moderado/`
       ),
       new Transtorno(
-        'Retardo Mental Grave (F72)',
+        'Desordem do Desenvolvimento Intelectual (6A00.Z)',
         `Como características do retardo mental grave pode-se destacar uma incapacidade de aprendizagem mesmo quando comparada a uma criança com um retardo menos intenso, especialmente nos casos em que o QI é inferior a 19. Nesses casos, em geral a criança pode não conseguir aprender a falar e necessita de muita ajuda para as atividades diárias, como higiene e alimentação, mesmo na vida adulta.`,
         `https://lobo.jusbrasil.com.br/artigos/361100297/quais-as-classificacoes-do-retardo-mental-e-como-repercute-no-ambito-penal`
       ),
       new Transtorno(
-        'Microcefalia (Q02)',
-        `Microcefalia é uma malformação congênita, em que o cérebro não se desenvolve de maneira adequada. Neste caso, os bebês nascem com perímetro cefálico (PC) menor que o normal, ou seja, igual ou inferior a 32 cm. Essa malformação congênita pode ser efeito de uma série de fatores de diferentes origens, como substâncias químicas e agentes biológicos (infecciosos), como bactérias, vírus e radiação.`,
-        `https://bvsms.saude.gov.br/microcefalia/`
+        'Microcefalia (LA05.0)',
+        `Condição causada pela falha da cabeça em se desenvolver corretamente durante o período pré-natal. Esta condição é caracterizada por um tamanho de cabeça significativamente menor que o normal para sua idade e sexo. Essa condição também pode apresentar atrasos no desenvolvimento, dificuldades de equilíbrio e coordenação, baixa estatura, hiperatividade, retardo mental, convulsões ou outras anormalidades neurológicas.`,
+        `https://icd.who.int/browse11/l-m/en#/http://id.who.int/icd/entity/179350437`
       ),
       new Transtorno(
-        'Epilepsia (G40)',
-        `É uma alteração temporária e reversível do funcionamento do cérebro, que não tenha sido causada por febre, drogas ou distúrbios metabólicos e se expressa por crises epilépticas repetidas.`,
-        `https://bvsms.saude.gov.br/epilepsia-6/`
+        'Epilepsia (G40.2)',
+        `Pelo menos 2 convulsões não provocadas (ou reflexas) ocorrendo com mais de 24 horas de intervalo.`,
+        `https://icd.who.int/browse11/l-m/en#/http%3a%2f%2fid.who.int%2ficd%2fentity%2f1397288146`
       ),
       new Transtorno(
-        'Transtorno Hipercinéticos (F90)',
-        `Esta subclasse de distúrbios é caracterizada pelo comportamento social disruptivo e é, frequentemente, mais penoso para outros do que para as próprias pessoas com o distúrbio. O transtorno hipercinético refere-se a "muito movimento", a inquietação, impulsividade, agitação. É um dos componentes do Transtorno de Déficit de Atenção e Hiperatividade, ou TDAH. Alguns podem ser mais hipercinéticos ou hiperativos, ou impulsivos, ou desatentos.`,
-        `https://treinamento24.com/library/lecture/read/748521-o-que-e-f90-transtornos-hipercineticos`
+        'Transtorno do Déficit de Atenção e Hiperatividade (6A05)',
+        `O transtorno de déficit de atenção e hiperatividade é caracterizado por um padrão persistente (pelo menos 6 meses) de desatenção e/ou hiperatividade-impulsividade que tem um impacto negativo direto no funcionamento acadêmico, ocupacional ou social. Há evidências de sintomas significativos de desatenção e/ou hiperatividade-impulsividade antes dos 12 anos de idade, tipicamente entre o início e o meio da infância, embora alguns indivíduos possam vir à atenção clínica mais tarde. O grau de desatenção e hiperatividade-impulsividade está fora dos limites da variação normal esperada para a idade e nível de funcionamento intelectual.`,
+        `https://icd.who.int/browse11/l-m/en#/http%3a%2f%2fid.who.int%2ficd%2fentity%2f821852937`
       ),
       new Transtorno(
-        'Sindrome de Down (Q90)',
-        `A síndrome de Down, ou trissomia 21, é uma doença genética causada por uma mutação no cromossomo 21 que faz com que o portador não tenha um par, mas um trio de cromossomos, e por isso no total não possui 46 cromossomos, mas 47.
-        Essa alteração do cromossomo 21 faz com que a criança nasça com características específicas, como implantação mais baixa das orelhas, olhos puxadinhos para cima e língua grande, por exemplo. Conheça outras características dessa síndrome.`,
-        `https://www.tuasaude.com/sindrome-de-down/`
+        'Trissomia 21 Completa (ou Sindrome de Down) (LD40.0)',
+        `A trissomia 21 é uma anomalia cromossômica, caracterizada pela presença de uma terceira cópia (parcial ou total) do cromossomo 21, cujas manifestações clínicas incluem deficiência intelectual variável, hipotonia muscular e frouxidão articular, muitas vezes associada a dismorfismo facial e malformações variáveis (essencialmente cardíaca e digestivo) e risco de complicações (epilepsia, leucemia, patologias auto-imunes e endócrinas, envelhecimento precoce e doença de Alzheimer.`,
+        `https://icd.who.int/browse11/l-m/en#/http://id.who.int/icd/entity/1624623908`
       ),
       new Transtorno(
-        'Síndrome de Rett - TGO (F84.2)',
-        `A síndrome de Rett é uma condição genética rara que afeta principalmente as meninas e que prejudica o sistema nervoso central, causando atraso no desenvolvimento físico e neurológico, gerando características e sintomas típicos, como irritabilidade frequente, convulsões e movimentos involuntários.`,
-        `https://www.tuasaude.com/caracteristicas-da-sindrome-de-rett/`
+        'Síndrome de Rett - (LD90.4)',
+        `Condição em que o desenvolvimento inicial aparentemente normal é seguido por perda parcial ou completa da fala e das habilidades de locomoção e uso das mãos, juntamente com desaceleração do crescimento da cabeça, geralmente com início entre sete e 24 meses de idade. Perda de movimentos intencionais das mãos, estereotipias de torcer as mãos e hiperventilação são características.`,
+        `https://icd.who.int/browse11/l-m/en#/http://id.who.int/icd/entity/201200685`
       ),
       new Transtorno(
-        'Esquizofrenia Residual (F20.5)',
-        `É uma forma crônica das doenças. Acontece quando os critérios para esquizofrenia ocorreram no passado, mas não estão ativos atualmente, entretanto, ainda persistem sintomas negativos como lentificação, isolamento social, falta de iniciativa ou afeição, expressão facial diminuída ou falta de autocuidado, por exemplo.`,
-        `https://www.tuasaude.com/esquizofrenia/`
+        'Esquizofrenia(6A20.Z)',
+        `A esquizofrenia é caracterizada por distúrbios em múltiplas modalidades mentais, incluindo pensamento (por exemplo, delírios, desorganização na forma de pensamento), percepção (por exemplo, alucinações), autoexperiência (por exemplo, a experiência de que os sentimentos, impulsos, pensamentos ou comportamento estão sob o controle de uma força externa), cognição (p. parece respostas emocionais bizarras ou sem propósito, imprevisíveis ou inadequadas que interferem na organização do comportamento).`,
+        `https://icd.who.int/browse11/l-m/en#/http%3a%2f%2fid.who.int%2ficd%2fentity%2f1683919430?data=%7B%22dataType%22%3A%22pc%22%2C%22postcoordinationCodeSet%22%3A%7B%22stemId%22%3A%22http%3A%2F%2Fid.who.int%2Ficd%2Fentity%2F1683919430%22%2C%22axisToValueIds%22%3A%7B%22hasManifestation%22%3A%5B%7B%22stemId%22%3A%22http%3A%2F%2Fid.who.int%2Ficd%2Fentity%2F1358754380%22%7D%5D%7D%7D%7D`
       ),
       new Transtorno(
-        'Cegueira (H54.0)',
-        `A perda da visão, também chamada de cegueira, pode ser ocasionada por vários fatores e aparecer de variadas formas. A cegueira pode ser congênita (quando já nasce com a pessoa) ou adquirida, que pode ser causada por diferentes fatores.
-        Existe também a cegueira resultante de acidentes com objetos pontiagudos, queimaduras, substâncias químicas e inflamáveis. Rubéola, sarampo e toxoplasmose em mulheres grávidas também podem ocasionar cegueira no bebê.`,
-        `https://www.unimed.coop.br/viver-bem/saude-em-pauta/cegueira`
+        'Cegueira (9D90.4)',
+        `A perda da visão, também chamada de cegueira, pode ser ocasionada por vários fatores e aparecer de variadas formas. A cegueira pode ser congênita (quando já nasce com a pessoa) ou adquirida, que pode ser causada por diferentes fatores. Existe também a cegueira resultante de acidentes com objetos pontiagudos, queimaduras, substâncias químicas e inflamáveis. Rubéola, sarampo e toxoplasmose em mulheres grávidas também podem ocasionar cegueira no bebê.`,
+        `https://icd.who.int/browse11/l-m/en#/http%3a%2f%2fid.who.int%2ficd%2fentity%2f1103667651`
       ),
       new Transtorno(
-        'Transtorno mental e comportamental devido ao uso de canabinóides (F12)',
-        `Estudos mostram a relação entre o abuso de Cannabis sativa e o surgimento de possíveis psicoses desencadeadas. Além disso, os mesmos indicam que o abuso contínuo da maconha pode contribuir para desenvolver doenças como esquizofrenia e outros sintomas psicóticos, além de transtornos ansiosos. De acordo com Diehl; Cordeiro; Laranjeira (2010), este risco é de 1,2 a 2,8 (95% IC, OddsRatio) em indivíduos vulneráveis, ou seja, que já possuem uma predisposição. Tais resultados são advindos de estudos longitudinais que relacionaram temporalidade, causalidade e vulnerabilidade biológica. Com isso, foi concluído pelos estudos que o abuso da Cannabis sativa pode ser considerado um dos elementos de causalidade que leva à esquizofrenia na fase adulta.`,
-        `http://pensaracademico.unifacig.edu.br/index.php/semiariocientifico/article/viewFile/1334/1139`
+        'Transtorno mental e comportamental devido ao uso de canabinóides (6C41)',
+        `Os transtornos devidos ao uso de substâncias incluem transtornos que resultam de uma única ocasião ou uso repetido de substâncias que possuem propriedades psicoativas, incluindo certos medicamentos. Estão incluídos os transtornos relacionados a quatorze classes ou grupos de substâncias psicoativas. Normalmente, o uso inicial dessas substâncias produz efeitos psicoativos agradáveis ou atraentes que são recompensadores e reforçados com o uso repetido. Com o uso continuado, muitas das substâncias incluídas têm a capacidade de produzir dependência.`,
+        `https://icd.who.int/browse11/l-m/en#/http%3a%2f%2fid.who.int%2ficd%2fentity%2f590211325`
       ),
     ]
   }
