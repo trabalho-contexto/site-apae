acf9464a55756069929f54b1a083055842e326a7
Leonam Teixeira de Vasconcelos <leonam.teixeira.vasconcelos@gmail.com>
2022-02-07 22:07:24 -0300

    fix: ajuste post

--- apae/src/modelo/post.modelo.ts
+++ apae/src/modelo/post.modelo.ts
@@ -3,16 +3,19 @@ export default class Post {
     titulo: string
     descricao: string
     data: string
+    caracteresAOcultar: number
 
     constructor (
         imgLink: string = '',
         titulo: string = '',
         descricao: string = '',
         data: string = '',
+        caracteresAOcultar: number = 150
     ) {
         this.imgLink = imgLink       
         this.titulo = titulo       
         this.descricao = descricao       
         this.data = data       
+        this.caracteresAOcultar = caracteresAOcultar
     }
 }
